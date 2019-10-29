a9e64ea96c6a2b4d9de2db6f9392501d0b4d417e
Anthony Green <green@moxielogic.com>
2019-10-29 12:10:43 -0400

    Don't uuencode.  Print log location.  Indicate PASS or FAIL

--- cfarm-test-libffi.sh
+++ cfarm-test-libffi.sh
@@ -4,25 +4,22 @@ DIR=$(mktemp -d $HOME/libffi.XXXXXX)
 
 echo Testing git commit $1
 
-(cd $DIR
- git clone https://github.com/libffi/libffi
- cd libffi
- git checkout $1
- ./autogen.sh
- ./configure
- make
- make check
- EXIT_CODE=$?
- gzip -c -9 */testsuite/libffi.log > libffi.log.gz
- echo ================================================================
- echo The logs are too long for travis to handle, so we compress and
- echo uuencode them.  Download, decode and uncompress if you need to
- echo read them.  For example, if you select and save this text
- echo as libffi.uu, run: 'cat libffi.uu | uudecode | gzip -d | less'.
- echo ================================================================
- uuencode libffi.log.gz -)
-
+cd $DIR
+git clone https://github.com/libffi/libffi
+cd libffi
+git checkout $1
+./autogen.sh
+./configure
+make
+make check
+EXIT_CODE=$?
+gzip -c -9 */testsuite/libffi.log > libffi.log.gz
+echo ==LOGFILE== `pwd`/libffi.log.gz
+cd ..
 rm -rf $DIR
 
-echo EXIT_CODE = $EXIT_CODE
-
+if test $EXIT_CODE; then
+    echo ==PASS==;
+else
+    echo ==FAIL==;
+fi
